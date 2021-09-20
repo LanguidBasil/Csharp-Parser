1a73e09b3cd9a5bc0df0d24f104fc57797ff3997
LanguidBasil <e.makunev@gmail.com>
2021-09-20 17:37:18 +1000

    Created layout for movie parsing

--- Csharp-Parser/FormMain.Designer.cs
+++ Csharp-Parser/FormMain.Designer.cs
@@ -29,28 +29,101 @@ namespace Csharp_Parser
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+			this.PreviewTextBox = new System.Windows.Forms.TextBox();
+			this.PreviewLabel = new System.Windows.Forms.Label();
+			this.SaveLabel = new System.Windows.Forms.Label();
+			this.ParseButton = new System.Windows.Forms.Button();
+			this.Savebutton = new System.Windows.Forms.Button();
+			this.SaveResultLabel = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
-			// openFileDialog
+			// PreviewTextBox
 			// 
-			this.openFileDialog.FileName = "openFileDialog1";
+			this.PreviewTextBox.Location = new System.Drawing.Point(12, 41);
+			this.PreviewTextBox.Multiline = true;
+			this.PreviewTextBox.Name = "PreviewTextBox";
+			this.PreviewTextBox.ReadOnly = true;
+			this.PreviewTextBox.Size = new System.Drawing.Size(281, 151);
+			this.PreviewTextBox.TabIndex = 0;
+			this.PreviewTextBox.TabStop = false;
+			// 
+			// PreviewLabel
+			// 
+			this.PreviewLabel.AutoSize = true;
+			this.PreviewLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.PreviewLabel.Location = new System.Drawing.Point(8, 12);
+			this.PreviewLabel.Name = "PreviewLabel";
+			this.PreviewLabel.Size = new System.Drawing.Size(72, 19);
+			this.PreviewLabel.TabIndex = 1;
+			this.PreviewLabel.Text = "Preview";
+			// 
+			// SaveLabel
+			// 
+			this.SaveLabel.AutoSize = true;
+			this.SaveLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.SaveLabel.Location = new System.Drawing.Point(8, 211);
+			this.SaveLabel.Name = "SaveLabel";
+			this.SaveLabel.Size = new System.Drawing.Size(117, 19);
+			this.SaveLabel.TabIndex = 2;
+			this.SaveLabel.Text = "Save Result:";
+			// 
+			// ParseButton
+			// 
+			this.ParseButton.Font = new System.Drawing.Font("Consolas", 10F);
+			this.ParseButton.Location = new System.Drawing.Point(69, 253);
+			this.ParseButton.Name = "ParseButton";
+			this.ParseButton.Size = new System.Drawing.Size(75, 23);
+			this.ParseButton.TabIndex = 3;
+			this.ParseButton.Text = "Parse";
+			this.ParseButton.UseVisualStyleBackColor = true;
+			// 
+			// Savebutton
+			// 
+			this.Savebutton.Font = new System.Drawing.Font("Consolas", 10F);
+			this.Savebutton.Location = new System.Drawing.Point(156, 253);
+			this.Savebutton.Name = "Savebutton";
+			this.Savebutton.Size = new System.Drawing.Size(75, 23);
+			this.Savebutton.TabIndex = 4;
+			this.Savebutton.Text = "Save";
+			this.Savebutton.UseVisualStyleBackColor = true;
+			// 
+			// SaveResultLabel
+			// 
+			this.SaveResultLabel.AutoSize = true;
+			this.SaveResultLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.SaveResultLabel.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+			this.SaveResultLabel.Location = new System.Drawing.Point(131, 211);
+			this.SaveResultLabel.Name = "SaveResultLabel";
+			this.SaveResultLabel.Size = new System.Drawing.Size(90, 19);
+			this.SaveResultLabel.TabIndex = 5;
+			this.SaveResultLabel.Text = "Not Saved";
 			// 
 			// FormMain
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(946, 547);
-			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Controls.Add(this.SaveResultLabel);
+			this.Controls.Add(this.Savebutton);
+			this.Controls.Add(this.ParseButton);
+			this.Controls.Add(this.SaveLabel);
+			this.Controls.Add(this.PreviewLabel);
+			this.Controls.Add(this.PreviewTextBox);
+			this.Margin = new System.Windows.Forms.Padding(2);
 			this.Name = "FormMain";
 			this.Text = "Tag Editor";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
-
-		private System.Windows.Forms.OpenFileDialog openFileDialog;
+		private System.Windows.Forms.TextBox PreviewTextBox;
+		private System.Windows.Forms.Label PreviewLabel;
+		private System.Windows.Forms.Label SaveLabel;
+		private System.Windows.Forms.Button ParseButton;
+		private System.Windows.Forms.Button Savebutton;
+		private System.Windows.Forms.Label SaveResultLabel;
 	}
 }
 
--- Csharp-Parser/FormMain.cs
+++ Csharp-Parser/FormMain.cs
@@ -1,5 +1,4 @@
 ﻿using System.Windows.Forms;
-using TagLib;
 
 namespace Csharp_Parser
 {
