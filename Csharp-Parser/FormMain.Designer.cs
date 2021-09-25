9e69238b2545810c82dd6b524e723184fce899e5
LanguidBasil <e.makunev@gmail.com>
2021-09-25 19:41:25 +1000

    Set up html loading

--- Csharp-Parser/FormMain.Designer.cs
+++ Csharp-Parser/FormMain.Designer.cs
@@ -29,101 +29,90 @@ namespace Csharp_Parser
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.PreviewTextBox = new System.Windows.Forms.TextBox();
-			this.PreviewLabel = new System.Windows.Forms.Label();
-			this.SaveLabel = new System.Windows.Forms.Label();
-			this.ParseButton = new System.Windows.Forms.Button();
-			this.Savebutton = new System.Windows.Forms.Button();
-			this.SaveResultLabel = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// PreviewTextBox
-			// 
-			this.PreviewTextBox.Location = new System.Drawing.Point(12, 41);
-			this.PreviewTextBox.Multiline = true;
-			this.PreviewTextBox.Name = "PreviewTextBox";
-			this.PreviewTextBox.ReadOnly = true;
-			this.PreviewTextBox.Size = new System.Drawing.Size(281, 151);
-			this.PreviewTextBox.TabIndex = 0;
-			this.PreviewTextBox.TabStop = false;
-			// 
-			// PreviewLabel
-			// 
-			this.PreviewLabel.AutoSize = true;
-			this.PreviewLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.PreviewLabel.Location = new System.Drawing.Point(8, 12);
-			this.PreviewLabel.Name = "PreviewLabel";
-			this.PreviewLabel.Size = new System.Drawing.Size(87, 23);
-			this.PreviewLabel.TabIndex = 1;
-			this.PreviewLabel.Text = "Preview";
-			// 
-			// SaveLabel
-			// 
-			this.SaveLabel.AutoSize = true;
-			this.SaveLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.SaveLabel.Location = new System.Drawing.Point(8, 211);
-			this.SaveLabel.Name = "SaveLabel";
-			this.SaveLabel.Size = new System.Drawing.Size(142, 23);
-			this.SaveLabel.TabIndex = 2;
-			this.SaveLabel.Text = "Save Result:";
-			// 
-			// ParseButton
-			// 
-			this.ParseButton.Font = new System.Drawing.Font("Consolas", 10F);
-			this.ParseButton.Location = new System.Drawing.Point(64, 253);
-			this.ParseButton.Name = "ParseButton";
-			this.ParseButton.Size = new System.Drawing.Size(75, 30);
-			this.ParseButton.TabIndex = 3;
-			this.ParseButton.Text = "Parse";
-			this.ParseButton.UseVisualStyleBackColor = true;
-			// 
-			// Savebutton
-			// 
-			this.Savebutton.Font = new System.Drawing.Font("Consolas", 10F);
-			this.Savebutton.Location = new System.Drawing.Point(156, 253);
-			this.Savebutton.Name = "Savebutton";
-			this.Savebutton.Size = new System.Drawing.Size(75, 30);
-			this.Savebutton.TabIndex = 4;
-			this.Savebutton.Text = "Save";
-			this.Savebutton.UseVisualStyleBackColor = true;
-			// 
-			// SaveResultLabel
-			// 
-			this.SaveResultLabel.AutoSize = true;
-			this.SaveResultLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.SaveResultLabel.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-			this.SaveResultLabel.Location = new System.Drawing.Point(152, 211);
-			this.SaveResultLabel.Name = "SaveResultLabel";
-			this.SaveResultLabel.Size = new System.Drawing.Size(109, 23);
-			this.SaveResultLabel.TabIndex = 5;
-			this.SaveResultLabel.Text = "Not Saved";
-			// 
-			// FormMain
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(946, 547);
-			this.Controls.Add(this.SaveResultLabel);
-			this.Controls.Add(this.Savebutton);
-			this.Controls.Add(this.ParseButton);
-			this.Controls.Add(this.SaveLabel);
-			this.Controls.Add(this.PreviewLabel);
-			this.Controls.Add(this.PreviewTextBox);
-			this.Margin = new System.Windows.Forms.Padding(2);
-			this.Name = "FormMain";
-			this.Text = "Tag Editor";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.PreviewTextBox = new System.Windows.Forms.TextBox();
+            this.PreviewLabel = new System.Windows.Forms.Label();
+            this.SiteLabel = new System.Windows.Forms.Label();
+            this.GetCodeButton = new System.Windows.Forms.Button();
+            this.SiteTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // PreviewTextBox
+            // 
+            this.PreviewTextBox.Location = new System.Drawing.Point(16, 50);
+            this.PreviewTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.PreviewTextBox.Multiline = true;
+            this.PreviewTextBox.Name = "PreviewTextBox";
+            this.PreviewTextBox.ReadOnly = true;
+            this.PreviewTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.PreviewTextBox.Size = new System.Drawing.Size(373, 185);
+            this.PreviewTextBox.TabIndex = 0;
+            this.PreviewTextBox.TabStop = false;
+            // 
+            // PreviewLabel
+            // 
+            this.PreviewLabel.AutoSize = true;
+            this.PreviewLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PreviewLabel.Location = new System.Drawing.Point(11, 15);
+            this.PreviewLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.PreviewLabel.Name = "PreviewLabel";
+            this.PreviewLabel.Size = new System.Drawing.Size(72, 19);
+            this.PreviewLabel.TabIndex = 1;
+            this.PreviewLabel.Text = "Preview";
+            // 
+            // SiteLabel
+            // 
+            this.SiteLabel.AutoSize = true;
+            this.SiteLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SiteLabel.Location = new System.Drawing.Point(11, 260);
+            this.SiteLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.SiteLabel.Name = "SiteLabel";
+            this.SiteLabel.Size = new System.Drawing.Size(54, 19);
+            this.SiteLabel.TabIndex = 2;
+            this.SiteLabel.Text = "URL: ";
+            // 
+            // GetCodeButton
+            // 
+            this.GetCodeButton.Font = new System.Drawing.Font("Consolas", 10F);
+            this.GetCodeButton.Location = new System.Drawing.Point(155, 308);
+            this.GetCodeButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.GetCodeButton.Name = "GetCodeButton";
+            this.GetCodeButton.Size = new System.Drawing.Size(100, 37);
+            this.GetCodeButton.TabIndex = 3;
+            this.GetCodeButton.Text = "Get code";
+            this.GetCodeButton.UseVisualStyleBackColor = true;
+            this.GetCodeButton.Click += new System.EventHandler(this.GetCodeButton_Click);
+            // 
+            // SiteTextBox
+            // 
+            this.SiteTextBox.Location = new System.Drawing.Point(72, 260);
+            this.SiteTextBox.Name = "SiteTextBox";
+            this.SiteTextBox.Size = new System.Drawing.Size(317, 22);
+            this.SiteTextBox.TabIndex = 5;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 363);
+            this.Controls.Add(this.SiteTextBox);
+            this.Controls.Add(this.GetCodeButton);
+            this.Controls.Add(this.SiteLabel);
+            this.Controls.Add(this.PreviewLabel);
+            this.Controls.Add(this.PreviewTextBox);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "FormMain";
+            this.Text = "Tag Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
 		#endregion
 		private System.Windows.Forms.TextBox PreviewTextBox;
 		private System.Windows.Forms.Label PreviewLabel;
-		private System.Windows.Forms.Label SaveLabel;
-		private System.Windows.Forms.Button ParseButton;
-		private System.Windows.Forms.Button Savebutton;
-		private System.Windows.Forms.Label SaveResultLabel;
-	}
+		private System.Windows.Forms.Label SiteLabel;
+		private System.Windows.Forms.Button GetCodeButton;
+        private System.Windows.Forms.TextBox SiteTextBox;
+    }
 }
 
--- Csharp-Parser/FormMain.cs
+++ Csharp-Parser/FormMain.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Forms;
+using System.Net.Http;
 
 namespace Csharp_Parser
 {
@@ -8,5 +9,16 @@ namespace Csharp_Parser
 		{
 			InitializeComponent();
 		}
-	}
+
+        private async void GetCodeButton_Click(object sender, System.EventArgs e)
+        {
+			HttpClient client = new HttpClient();
+			client.DefaultRequestHeaders.Add("User-Agent", "C# Parser");
+
+			string content = await client.GetStringAsync(SiteTextBox.Text);
+
+			PreviewTextBox.SelectionStart = PreviewTextBox.Text.Length;
+			PreviewTextBox.Text = content;
+		}
+    }
 }
